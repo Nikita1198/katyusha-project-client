d8b6271a47e03c39efe2198b5ee0c5ef0f98cbd9
Nikita1198 <ivanov.n.2019@gmail.com>
2024-03-06 12:26:03 +0300

    Add YMInitializer

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { SpeedInsights } from "@vercel/speed-insights/react"
-import { Analytics } from "@vercel/analytics/react"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
+import { YMInitializer } from "react-yandex-metrika";
+import YOUR_COUNTER_ID from "../config.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <YMInitializer accounts={[YOUR_COUNTER_ID]} options={{ webvisor: true }} />
     <App />
     <SpeedInsights />
     <Analytics />
   </React.StrictMode>,
-)
+);
